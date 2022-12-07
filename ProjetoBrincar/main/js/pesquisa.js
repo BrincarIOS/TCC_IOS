c1ae9e183d668adf6e71ec266815e8fee2cb884b
Guilherme <francaguilherme27@gmail.com>
2022-12-07 14:25:27 -0300

    Produção

--- ProjetoBrincar/main/js/pesquisa.js
+++ ProjetoBrincar/main/js/pesquisa.js
@@ -1,15 +1,28 @@
-let pesquisa = document.getElementById('pesquisa')
-let lista = document.getElementsByClassName('container-texto');
-var item = document.getElementsByClassName('tipo');
-
-console.log(ite);
-console.log(lista)
-
-function buscar(){
-    for(i = 0; i < lista.length; i++){
-        let valor = lista[i].getElementsByTagName('span');
-        console.log(document.getElementsByClassName('tipo'));
-    }
-}
+let pesquisa = document.getElementById('pesquisa');
+let lista = document.getElementsByClassName('containerCard01');
 
 pesquisa.addEventListener('keyup', buscar);
+
+function buscar() {
+    let input = document.getElementById('pesquisa').value;
+    input = input.toLowerCase();
+    console.log(input);
+    if(input == ''){
+        for(i = 0; i < lista.length; i++){
+            lista[i].style.display = 'flex'
+        }
+    }
+    for (i = 0; i < lista.length; i++) {
+        let texto = lista[i];
+        texto = texto.value;
+        console.log(texto);
+        interno: for (j = 0; j < input.length; j++) {
+            if (input[j] == texto[j]) {
+                lista[i].style.display = 'flex';
+            } else {
+                lista[i].style.display = 'none';
+                break interno;
+            }
+        }
+    }
+}
