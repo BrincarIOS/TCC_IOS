74f64dba0c0eb148d45aa4268bc638d16da16c29
Guilherme <francaguilherme27@gmail.com>
2022-12-08 09:26:44 -0300

    Conclusão

--- ProjetoBrincar/main/js/pesquisa.js
+++ ProjetoBrincar/main/js/pesquisa.js
@@ -6,18 +6,17 @@ pesquisa.addEventListener('keyup', buscar);
 function buscar() {
     let input = document.getElementById('pesquisa').value;
     input = input.toLowerCase();
-    console.log(input);
     if(input == ''){
         for(i = 0; i < lista.length; i++){
             lista[i].style.display = 'flex'
         }
     }
     for (i = 0; i < lista.length; i++) {
-        let texto = lista[i];
-        texto = texto.value;
-        console.log(texto);
+        let texto = document.getElementById("b" + `${i}`);
+        texto = texto.textContent;
+        texto = texto.toLowerCase();
         interno: for (j = 0; j < input.length; j++) {
-            if (input[j] == texto[j]) {
+            if (texto[j] == input[j]) {
                 lista[i].style.display = 'flex';
             } else {
                 lista[i].style.display = 'none';
